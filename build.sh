d0b357d6641df0fe754e8105758f42707d5b4b23
Mahendran Kannan <kannanm@who.int>
2021-08-25 13:58:55 +0530

    👨‍🔧 FIX - Deployment issues - remove sleep👨‍🔧

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-# quay.io/whoacademy/badgr-server:latest
+# quay.io/whoacademy/badgr-server:0.2.0
 
-version='0.1.0'
+version='0.2.0'
 
 docker build -t quay.io/whoacademy/badgr-server:$version -f ./.docker/Dockerfile.openshift.test.api .
